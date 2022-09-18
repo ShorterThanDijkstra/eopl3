a84ef5cfb53a781c0ced95027f69cd502374d270
maerd-zinbieL <db1345569965@gmail.com>
2022-09-18 15:04:18 +0800

    exercise 2.2, faster

--- code/scheme/ch2/exercise2.2.rkt
+++ code/scheme/ch2/exercise2.2.rkt
@@ -28,20 +28,29 @@
   (lambda (d-tree)
     (= (diff-tree->number d-tree) 0)))
 
+; (define successor
+;   (lambda (d-tree)
+;     (let [(two (make-diff-tree '(one) (make-diff-tree (zero) '(one))))]
+;       (if (equal? d-tree '(one))
+;           two
+;           (make-diff-tree (successor (diff-tree-left d-tree))
+;                           (diff-tree-right d-tree))))))
+
 (define successor
   (lambda (d-tree)
-    (let [(two (make-diff-tree '(one) (make-diff-tree (zero) '(one))))]
-      (if (equal? d-tree '(one))
-          two
-          (make-diff-tree (successor (diff-tree-left d-tree))
-                          (diff-tree-right d-tree))))))
+   (let [(minus-one (make-diff-tree (make-diff-tree '(one) '(one)) '(one)))]
+     (make-diff-tree d-tree minus-one))))
+
+; (define predecessor
+;   (lambda (d-tree)
+;     (if (equal? d-tree '(one))
+;         (zero)
+;         (make-diff-tree (predecessor (diff-tree-left d-tree))
+;                         (diff-tree-right d-tree)))))
 
 (define predecessor
   (lambda (d-tree)
-    (if (equal? d-tree '(one))
-        (zero)
-        (make-diff-tree (predecessor (diff-tree-left d-tree))
-                        (diff-tree-right d-tree)))))
+    (make-diff-tree d-tree '(one))))
 
 ; m + n = m - (-n)
 (define diff-tree-plus
@@ -78,27 +87,20 @@
         [else #f]))
 (check-equal? (check-predecessor (number->diff-tree MAX) MAX) #t)
 
-(define MAX-DIFF-PLUS 100)
-(define MIN-DIFF-PLUS -100)
+(define MAX-DIFF-PLUS 200)
+(define MIN-DIFF-PLUS -200)
 
-(define (check-diff-tree-plus-fix m-d-tree-fix n-d-tree m-num-fix n-num)
-  ; (begin
-  ;   (write (list m-num-fix n-num))
-  ;   (newline)
-    (cond
-      [(not (= (diff-tree->number (diff-tree-plus m-d-tree-fix n-d-tree)) (+ m-num-fix n-num))) #f]
-      [(= n-num MIN-DIFF-PLUS) #t]
-      [else (check-diff-tree-plus-fix m-d-tree-fix (predecessor n-d-tree) m-num-fix (- n-num 1))]))
-    ; )
-
+(define MAX-DIFF-TREE (number->diff-tree MAX-DIFF-PLUS))
 (define (check-diff-tree-plus m-d-tree n-d-tree m-num n-num)
-  (if  (= m-num (- MIN-DIFF-PLUS 1)) #t
-       (if (check-diff-tree-plus-fix m-d-tree n-d-tree m-num n-num)
-           (check-diff-tree-plus (predecessor m-d-tree) n-d-tree (- m-num 1) n-num)
-           #f)))
-
-
-(check-equal? (check-diff-tree-plus (number->diff-tree MAX-DIFF-PLUS) (number->diff-tree MAX-DIFF-PLUS) MAX-DIFF-PLUS MAX-DIFF-PLUS) #t)
+  (begin
+    (write (list m-num n-num))
+    (newline)
+    (cond
+      [(not (= (diff-tree->number (diff-tree-plus m-d-tree n-d-tree)) (+ m-num n-num))) #f]
+      [(and (= m-num MIN-DIFF-PLUS) (= n-num MIN-DIFF-PLUS)) #t]
+      [(= m-num MIN-DIFF-PLUS) (check-diff-tree-plus MAX-DIFF-TREE (predecessor n-d-tree) MAX-DIFF-PLUS (- n-num 1))]
+      [else (check-diff-tree-plus (predecessor m-d-tree) n-d-tree (- m-num 1) n-num)])))
+(check-equal? (check-diff-tree-plus MAX-DIFF-TREE MAX-DIFF-TREE MAX-DIFF-PLUS MAX-DIFF-PLUS) #t)
 
 
 (define two  (make-diff-tree '(one) (make-diff-tree (zero) '(one))))
