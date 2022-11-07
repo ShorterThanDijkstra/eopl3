3ba05ba3042583e17d06bfb1c3822ad6b1d59191
maerd-zinbieL <db1345569965@gmail.com>
2022-11-07 18:39:16 +0800

    exercise 3.38

--- code/scheme/appendix-b/playground2.rkt
+++ code/scheme/appendix-b/playground2.rkt
@@ -26,11 +26,3 @@
 
 (define scan&parse
   (sllgen:make-string-parser the-lexical-spec the-grammar-spec))
-
-(define code1
-  "
-  letrec
-    even(x) = if zero?(x) then 1 else (odd -(x,1))
-    odd(x) = if zero?(x) then 0 else (even -(x,1))
-  in (odd 13)
-")
